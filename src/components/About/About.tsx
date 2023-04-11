c8efb7bf729206c780a30f2bce29c258f6596a61
Arnaldo Cisneros <arnaldo.jose.cisneros.zambrano@gmail.com>
2023-04-10 19:20:15 -0500

    Created About page and removed comments from HTML

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import './About.scss';
 
 const About = () => {
-  return <>ABOUT</>;
+  return (
+    <>
+      <h1 className="general__page_title">About</h1>
+      <h2 className="general__section_divider">About me</h2>
+      <p>
+        I was born in the city of Caracas, Venezuela. When i'm not working in my
+        computer, I like to play piano, read books or play videogames. I really
+        like traveling and making new friends, and I'd rather go to dinner with
+        them, than going to parties and loud places. I love to have deep
+        conversations with other people. And, last but not least, I'm more of a
+        cat person myself.
+      </p>
+      <h2 className="general__section_divider">About my work</h2>
+      <p>
+        I like to put heart on every project I begin, even more on projects
+        where I can show some creativity. I've done websites, desktop
+        applications, and even small videogames on Unity. I like to have a full
+        picture of the project I'm going to start before writing the first lines
+        of code. If I really get invested in what I'm doing, I can be a bit of a
+        perfectionist.
+      </p>
+      <h2 className="general__section_divider">About my expertise</h2>
+      <p>
+        Since highschool, i've always been interested in the process of creating
+        software. I started programing in BASIC, and slowly moved to C++, Java,
+        and finally Python and JavaScript. I consider myself very good using the
+        standards in web development, those being HTML, CSS and Javascript, but
+        I like to keep an open mind in terms of learning new things.
+      </p>
+      <h2 className="general__section_divider">About my education</h2>
+      <p>
+        These are some of my most important certificates. Click on each image to
+        see a description of the course. You can also see all my certificates in
+        the link below.
+      </p>
+    </>
+  );
 };
 
 export default About;
