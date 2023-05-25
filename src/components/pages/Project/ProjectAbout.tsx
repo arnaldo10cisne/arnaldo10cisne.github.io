f2ce7ce63537ee6d5e2b4c18fde96b334350dc60
Arnaldo Cisneros <arnaldo.jose.cisneros.zambrano@gmail.com>
2023-05-25 16:15:12 -0400

    Added styles to project description

--- src/components/pages/Project/ProjectAbout.tsx
+++ src/components/pages/Project/ProjectAbout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ProjectItem } from '../../../utilities/models';
 import ProjectInfoTable from './ProjectInfoTable';
+import './ProjectAbout.scss';
 
 interface ProjectAboutProps {
   project?: ProjectItem;
@@ -10,8 +11,9 @@ const ProjectAbout = ({ project }: ProjectAboutProps) => {
   return (
     <>
       <h2 className="global__section_divider">About this project</h2>
-      <div>
+      <div className="about_container">
         <div
+          className="project_description"
           dangerouslySetInnerHTML={{
             __html: project?.description_innerHTML || '',
           }}
