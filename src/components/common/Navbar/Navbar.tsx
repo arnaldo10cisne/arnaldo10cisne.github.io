51c3927e361b66ee484f77fbfa69434332c239ab
Arnaldo Cisneros <arnaldo.jose.cisneros.zambrano@gmail.com>
2023-05-25 13:32:08 -0400

    Fixed navbar bug and added logo

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -31,14 +31,25 @@ const Navbar = () => {
   return (
     <div className="navbar_container">
       <div className="logo">
-        <img src="" alt="" />
+        <a href="/">
+          <img
+            className="navbar_logo"
+            src="https://arnaldo10cisne.github.io/legacy_personal_website/assets/img/logo_small(3).png"
+            alt="logo"
+            width={'120px'}
+          />
+        </a>
       </div>
       <div className="menu">
         {navbarItems.map((item) => (
           <NavbarButton
             label={item.label}
             route={item.to}
-            selected={pathname.includes(item.to)}
+            selected={
+              item.label === 'Home'
+                ? pathname === item.to
+                : pathname.includes(item.to)
+            }
           />
         ))}
       </div>
