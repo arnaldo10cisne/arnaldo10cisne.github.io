6893551bad69ef925618cb2cbefb218f43fed817
Arnaldo Cisneros <arnaldo.jose.cisneros.zambrano@gmail.com>
2023-05-30 11:07:54 -0400

    Changed footer links logic

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -3,16 +3,29 @@ import './Footer.scss';
 
 const links = [
   {
+    label: 'Facebook',
+    url: 'https://www.facebook.com/arnaldo10cisne/',
+    show: false,
+  },
+  {
     label: 'Instagram',
     url: 'https://www.instagram.com/arnaldo10cisne/',
+    show: false,
+  },
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/arnaldo10cisne',
+    show: false,
   },
   {
     label: 'Github',
     url: 'https://github.com/arnaldo10cisne',
+    show: true,
   },
   {
     label: 'Linkedin',
     url: 'https://www.linkedin.com/in/arnaldo10cisne/',
+    show: true,
   },
 ];
 
@@ -21,21 +34,25 @@ const Footer = () => {
     <div className="footer">
       <div className="social_media">
         <p>
-          {links.map((link) => (
-            <>
-              <a
-                className="social_media__link"
-                href={link.url}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {link.label}
-              </a>
-              {links.indexOf(link) !== links.length - 1 && (
-                <span className="linkSeparator"> | </span>
-              )}
-            </>
-          ))}
+          {links.map((link) => {
+            if (link.show) {
+              return (
+                <>
+                  <a
+                    className="social_media__link"
+                    href={link.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {link.label}
+                  </a>
+                  {links.indexOf(link) !== links.length - 1 && (
+                    <span className="linkSeparator"> | </span>
+                  )}
+                </>
+              );
+            }
+          })}
         </p>
       </div>
       <div className="copyright">
