35e831acde8874eca70acdfd7a9766fb3dd5352d
Arnaldo Cisneros <76781890+arnaldo10cisne@users.noreply.github.com>
2024-07-14 17:03:34 -0500

    Feature/seccion cloud projects (#5)
    * Created sections and moved getProjects outside of React component
    
    * used useQuery isLoading
    
    * Other improvements applied
    
    * Changed text to english

--- src/components/pages/Portfolio/Portfolio.tsx
+++ src/components/pages/Portfolio/Portfolio.tsx
@@ -2,41 +2,83 @@ import React from 'react';
 import './Portfolio.scss';
 import PageTitle from '../../common/PageTitle/PageTitle';
 import { useQuery } from 'react-query';
-import { FIREBASE_RTDB_URL, ProjectItem } from '../../../utilities/models';
+import {
+  FIREBASE_RTDB_URL,
+  ProjectItem,
+  PROJECT_TYPES,
+} from '../../../utilities/models';
 import LoadingSpinner from '../../utilities/LoadingSpinner/LoadingSpinner';
 import ProjectCard from '../../common/ProjectCard/ProjectCard';
 
-const Portfolio = () => {
-  const getProjects = async () =>
-    await fetch(
-      FIREBASE_RTDB_URL +
-        'projects.json?' +
-        new URLSearchParams({
-          orderBy: '"show"',
-          equalTo: 'true',
-        })
-    )
-      .then((response) => response.json())
-      .then((data) => Object.values(data) as ProjectItem[]);
+const getProjects = async () => {
+  const response = await fetch(
+    FIREBASE_RTDB_URL +
+      'projects.json?' +
+      new URLSearchParams({
+        orderBy: '"show"',
+        equalTo: 'true',
+      })
+  );
+  const data = await response.json();
+  return Object.values(data) as ProjectItem[];
+};
 
-  const { data: projects } = useQuery<ProjectItem[]>(
+const Portfolio = () => {
+  const { data: projects, isLoading } = useQuery<ProjectItem[]>(
     ['Portfolio', 'project_list'],
     getProjects
   );
 
+  const categorizedProjects = {
+    [PROJECT_TYPES.WEB]: [] as ProjectItem[],
+    [PROJECT_TYPES.CODE]: [] as ProjectItem[],
+    [PROJECT_TYPES.CLOUD]: [] as ProjectItem[],
+  };
+
+  if (projects) {
+    projects.forEach((project) => {
+      if (project.project_type) {
+        categorizedProjects[
+          project.project_type as keyof typeof categorizedProjects
+        ].push(project);
+      }
+    });
+  }
+
+  const renderProjects = (projects: ProjectItem[], title: string) => (
+    <>
+      <h2 className="global__section_divider">{title}</h2>
+      <div className="projectList global__page_container">
+        {projects.length > 0 ? (
+          projects.map((project) => (
+            <ProjectCard key={project.id} project={project} />
+          ))
+        ) : (
+          <p>There are no projects available in this category.</p>
+        )}
+      </div>
+    </>
+  );
+
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
+
   return (
     <>
       <PageTitle title="Portfolio" />
-      <h2 className="global__section_divider">Projects</h2>
-      <div className="projectList global__page_container">
-        {projects ? (
-          projects.map((project: ProjectItem) => (
-            <ProjectCard project={project} />
-          ))
-        ) : (
-          <LoadingSpinner />
-        )}
-      </div>
+      {renderProjects(
+        categorizedProjects[PROJECT_TYPES.CLOUD],
+        'Cloud Projects'
+      )}
+      {renderProjects(
+        categorizedProjects[PROJECT_TYPES.WEB],
+        'Web Development Projects'
+      )}
+      {renderProjects(
+        categorizedProjects[PROJECT_TYPES.CODE],
+        'Coding Projects'
+      )}
     </>
   );
 };
