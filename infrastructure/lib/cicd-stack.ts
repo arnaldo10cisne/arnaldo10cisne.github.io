415dcff8c0746a25cfe90c0b581b774d0582fb01
Arnaldo Cisneros <arnaldo.jose.cisneros.zambrano@gmail.com>
2024-11-09 16:11:57 -0500

    Created initial cloudfront distribution

--- infrastructure/lib/cicd-stack.ts
+++ infrastructure/lib/cicd-stack.ts
@@ -5,6 +5,12 @@ import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
 import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as iam from 'aws-cdk-lib/aws-iam';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as route53_targets from 'aws-cdk-lib/aws-route53-targets';
+import * as s3_deployment from 'aws-cdk-lib/aws-s3-deployment';
 
 interface PersonalWebsiteCICDStackProps extends cdk.StackProps {
   serverlessStackName: string;
@@ -140,6 +146,106 @@ export class PersonalWebsite_CICD_Stack extends cdk.Stack {
       ],
     });
 
+    const domainName = 'arnaldocisneros.com';
+    const hostedZone = route53.HostedZone.fromLookup(
+      this,
+      resourceName('HostedZone'),
+      {
+        domainName,
+      }
+    );
+
+    const certificate = new acm.Certificate(
+      this,
+      resourceName('SiteCertificate'),
+      {
+        certificateName: resourceName('SiteCertificate'),
+        domainName: domainName,
+        subjectAlternativeNames: [`*.${domainName}`],
+        validation: acm.CertificateValidation.fromDns(hostedZone),
+      }
+    );
+
+    // const originAccessIdentity = new cloudfront.OriginAccessIdentity(
+    //   this,
+    //   resourceName('OAI')
+    // );
+
+    // deploymentBucket.addToResourcePolicy(
+    //   new iam.PolicyStatement({
+    //     actions: ['s3:GetObject'],
+    //     resources: [deploymentBucket.arnForObjects('*')],
+    //     principals: [
+    //       new iam.CanonicalUserPrincipal(
+    //         originAccessIdentity.cloudFrontOriginAccessIdentityS3CanonicalUserId
+    //       ),
+    //     ],
+    //   })
+    // );
+
+    const distribution = new cloudfront.Distribution(
+      this,
+      resourceName('SiteDistribution'),
+      {
+        defaultRootObject: 'index.html',
+        domainNames: [domainName],
+        certificate,
+        defaultBehavior: {
+          origin: new origins.OriginGroup({
+            primaryOrigin:
+              origins.S3BucketOrigin.withOriginAccessIdentity(deploymentBucket),
+            fallbackOrigin: new origins.HttpOrigin(`www.${domainName}`),
+          }),
+          viewerProtocolPolicy:
+            cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        },
+      }
+    );
+
+    new route53.ARecord(this, resourceName('SiteAliasRecord'), {
+      recordName: domainName,
+      zone: hostedZone,
+      target: route53.RecordTarget.fromAlias(
+        new route53_targets.CloudFrontTarget(distribution)
+      ),
+    });
+
+    const invalidateCacheBuildProject = new codebuild.PipelineProject(
+      this,
+      resourceName('InvalidateCacheProject'),
+      {
+        buildSpec: codebuild.BuildSpec.fromObject({
+          version: 0.2,
+          phases: {
+            build: {
+              commands: [
+                `aws cloudfront create-invalidation --distribution-id ${distribution.distributionId} --paths "/*"`,
+              ],
+            },
+          },
+        }),
+      }
+    );
+    invalidateCacheBuildProject.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: ['cloudfront:CreateInvalidation'],
+        resources: [
+          `arn:aws:cloudfront::${cdk.Aws.ACCOUNT_ID}:distribution/${distribution.distributionId}`,
+        ],
+      })
+    );
+
+    pipeline.addStage({
+      stageName: 'InvalidateCache',
+      actions: [
+        new codepipeline_actions.CodeBuildAction({
+          actionName: 'InvalidateCache',
+          project: invalidateCacheBuildProject,
+          input: codeBuildArtifact,
+        }),
+      ],
+    });
+
     new cdk.CfnOutput(this, 'DeploymentBucketName', {
       value: deploymentBucket.bucketName,
     });
@@ -152,6 +258,10 @@ export class PersonalWebsite_CICD_Stack extends cdk.Stack {
     new cdk.CfnOutput(this, 'CertificatesTableARN', {
       value: `${props?.certificatesTableARN}`,
       description: 'Validation of ARN of the Certificates DynamoDB Table',
+    });
+
+    new cdk.CfnOutput(this, 'CloudFrontURL', {
+      value: `https://${distribution.domainName}`,
     });
   }
 }
